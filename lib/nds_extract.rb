ef5843dec8e1b1dc7e2a71ccb43410ae8f3dae01
Sara Fotros <sarafotros1@yahoo.com>
2019-11-14 13:05:06 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,8 +4,11 @@ require 'directors_database'
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
-  #
-  #
+   
+  require 'pp'
+  
+  pp nds
+   
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
   }
